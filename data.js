a70bd4155a8830683e17c298151ece4f2bddd5b5
Bala Logesh S <balassmp@gmail.com>
2021-08-06 12:58:21 +0530

    Implemented filtering

--- data.js
+++ data.js
@@ -1,4 +1,14 @@
-const data = [
+export const topics = [
+ 'All topics',
+ 'Agile Methodology',
+ 'SQL and NoSQL',
+ 'Unix',
+ 'Web Development',
+ 'Softwares',
+ 'Miscellaneous',
+]
+
+export const data = [
  {
   topic: 'Agile Methodology',
   links: [
@@ -15,73 +25,74 @@ const data = [
  {
   topic: 'SQL and NoSQL',
   links: [
-       {
+   {
     name: 'Differences between SQL and NoSQL DB',
     link: 'https://youtu.be/QwevGzVu_zk',
-      },
-       {
+   },
+   {
     name: 'NoSQL Types and details',
     link: 'https://www.guru99.com/nosql-tutorial.html',
-      },
-        {
+   },
+   {
     name: 'Introduction to NoSQL databases',
     link: 'https://youtu.be/uD3p_rZPBUQ',
-      },
-         {
+   },
+   {
     name: 'MongoDB Crash Course',
     link: 'https://youtu.be/-56x56UppqQ',
-      },
-          {
-    name: 'SQL - RPS material',
-    link: 'https://github.com/cmsanju/FSDJAVACLOUD/blob/master/Complete%20SQL.pptx.pdf',
    },
-  ]
-  
+   {
+    name: 'SQL - RPS material',
+    link:
+     'https://github.com/cmsanju/FSDJAVACLOUD/blob/master/Complete%20SQL.pptx.pdf',
+   },
+  ],
  },
  {
   topic: 'Unix',
   links: [
-       {
+   {
     name: 'Shell Scripting Crash Course',
     link: 'https://youtu.be/GtovwKDemnI',
    },
-  ]
+  ],
  },
  {
   topic: 'Web Development',
   links: [
-       {
+   {
     name: 'HTML CSS',
     link: 'https://www.freecodecamp.org/learn/responsive-web-design/',
-      },
-      {
+   },
+   {
     name: 'JavaScript',
-    link: 'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/',
-      },
-      {
+    link:
+     'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/',
+   },
+   {
     name: 'JavaScript Cheatsheet',
     link: 'https://github.com/mbeaudru/modern-js-cheatsheet',
    },
-  ]
+  ],
  },
  {
   topic: 'Softwares',
   links: [
-       {
+   {
     name: 'Adode XD Cracked Version',
-    link: 'https://youtu.be/GtovwKDemnIhttps://getintopc.com/softwares/adobe-xd-cc-2020-free-download-9770051/',
+    link:
+     'https://youtu.be/GtovwKDemnIhttps://getintopc.com/softwares/adobe-xd-cc-2020-free-download-9770051/',
    },
-  ]
+  ],
  },
  {
   topic: 'Miscellaneous',
   links: [
-       {
+   {
     name: 'Regex Cheatsheet',
-    link: 'https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285/',
+    link:
+     'https://medium.com/factory-mind/regex-tutorial-a-simple-cheatsheet-by-examples-649dc1c3f285/',
    },
-  ]
+  ],
  },
 ]
-
-export default data
--- script.js
+++ script.js
@@ -1,52 +1,71 @@
-import data from './data.js'
-
-const topics = [
- 'Agile Methodology',
- 'SQL and NoSQL',
- 'Unix',
- 'Web Development',
- 'Softwares',
- 'Miscellaneous',
-]
+import { data, topics } from './data.js'
 
 const filter = document.querySelector('#filter')
 const cards = document.querySelector('#cards')
 
-for (let i = 0; i < data.length; i++) {
- const item = data[i]
+// Function for populating the cards
+const populateCards = (data) => {
+    for (let i = 0; i < data.length; i++) {
+     const item = data[i]
 
- let ulElement = document.createElement('ul')
- for (let i = 0; i < item.links.length; i++) {
-    let liElement = document.createElement('li')
+     let ulElement = document.createElement('ul')
+     for (let i = 0; i < item.links.length; i++) {
+      let liElement = document.createElement('li')
 
-    let aElement = document.createElement('a')
-    aElement.href = item.links[i].link
-    aElement.target = '_blank'
-    aElement.innerText = item.links[i].name
-    liElement.appendChild(aElement)
-    ulElement.appendChild(liElement)
- }
+      let aElement = document.createElement('a')
+      aElement.href = item.links[i].link
+      aElement.target = '_blank'
+      aElement.innerText = item.links[i].name
+      liElement.appendChild(aElement)
+      ulElement.appendChild(liElement)
+     }
 
- let h2Element = document.createElement('h2')
- h2Element.innerText = item.topic
+     let h2Element = document.createElement('h2')
+     h2Element.innerText = item.topic
 
- let divElement = document.createElement('div')
- divElement.classList.add('card')
- divElement.id = item.topic
+     let divElement = document.createElement('div')
+     divElement.classList.add('card')
+     divElement.id = item.topic
 
- divElement.appendChild(h2Element)
- divElement.appendChild(ulElement)
+     divElement.appendChild(h2Element)
+     divElement.appendChild(ulElement)
 
- cards.appendChild(divElement)
+     cards.appendChild(divElement)
+    }
 }
 
-for (let i = 0; i < topics.length; i++) {
- let element = document.createElement('option')
- element.textContent = topics[i]
- element.value = topics[i]
- filter.appendChild(element)
+// Function for populating the select
+const populateSelect = () => {
+    for (let i = 0; i < topics.length; i++) {
+     let element = document.createElement('option')
+     element.textContent = topics[i]
+     element.value = topics[i]
+     filter.appendChild(element)
+    }
 }
 
+// Function to remove all the child elements
+const removeAllChildren = (parent) => {
+    while (parent.firstChild) {
+     parent.removeChild(parent.firstChild)
+    }
+} 
+
+// Invoking the functions
+populateSelect()
+populateCards(data)
+
+// Function to handle the select menu change
 const handleChange = () => {
- console.log(filter.options[filter.selectedIndex].value)
+    const selected = filter.options[filter.selectedIndex].value
+    if (selected === 'All topics') {
+     removeAllChildren(cards)
+     populateCards(data)
+    } else {
+     const newData = data.filter(datum => datum.topic === selected)
+     removeAllChildren(cards)
+     populateCards(newData)
+    }
 }
+
+filter.addEventListener('change', handleChange)
